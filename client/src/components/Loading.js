1514a9f50a1a9009f5ad1cc3883fa74233a56918
Lucas Mace <lucasmace4130@gmail.com>
2022-03-16 20:02:30 -0500

    Adding Shameless Plug

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,16 +1,21 @@
 import * as React from 'react';
 
 import Spinner from "../svg/spinner.svg";
-import { CenteredColContainer, HeadlineOne, CenterPara } from './styles/SSOT.style';
+import { CenteredColContainer, HeadlineOne, CenterPara, CreatedBy } from './styles/SSOT.style';
 import { LoadingSpinner } from './styles/Loading.style';
 
 const Loading = (props) => {
     return (
-        <CenteredColContainer>
-            <HeadlineOne>Loading...</HeadlineOne>
-            <LoadingSpinner src={Spinner} alt="Spinner Bicycle Wheel" />
-            <CenterPara>If the wheel just keeps on spinning, the server is overloaded. Please try again in about 30 minutes.</CenterPara>
-        </CenteredColContainer>
+        <>
+            <CenteredColContainer>
+                <HeadlineOne>Loading...</HeadlineOne>
+                <LoadingSpinner src={Spinner} alt="Spinner Bicycle Wheel" />
+                <CenterPara>If the wheel just keeps on spinning, the server is overloaded. Please try again in about 30 minutes.</CenterPara>
+            </CenteredColContainer>
+            <CenteredColContainer>
+                <CreatedBy>Created by <a href="http://www.lucasmace.com/" target="_blank" rel="noreferrer">Lucas Mace</a></CreatedBy>
+            </CenteredColContainer>
+        </>
     )
 }
 
